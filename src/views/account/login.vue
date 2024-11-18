9b3c85f791049787155b8217a816a6e043606323
ridhomckinnon <ridhoadha24@gmail.com>
2024-11-18 15:05:35 +0700

    fix save and add form

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -22,8 +22,8 @@ export default {
     data() {
         return {
             showPassword: false,
-            email: "admin@themesbrand.com",
-            password: "123456",
+            email: "013ichsanm@gmail.com",
+            password: "p@ssword123",
             submitted: false,
             authError: null,
             tryingToLogIn: false,
@@ -63,23 +63,34 @@ export default {
         ...authMethods,
         ...authFackMethods,
         ...notificationMethods,
+        // Fungsi untuk fokus ke input selanjutnya
+        focusNext(index) {
+            if (this.otp[index].length === 1 && index < this.otp.length - 1) {
+                this.$refs[`otp${index + 1}`][0].focus();
+            }
+        },
+        // Fungsi untuk fokus ke input sebelumnya saat backspace
+        focusPrev(index, event) {
+            if (event.key === 'Backspace' && index > 0 && !this.otp[index]) {
+                this.$refs[`otp${index - 1}`][0].focus();
+            }
+        },
 
         async signinapi() {
             this.typeRequestOtp = 'login';
             this.processing = true;
-            await axios.post(process.env.VUE_APP_API_URL + "/cms/v1/auth/login", {
-                email: this.email,
+            await axios.post(process.env.VUE_APP_API_URL + "/v1/auth/login", {
+                emailOrWa: this.email,
                 password: this.password
             }).then(response => {
-                localStorage.setItem('jwt', response.data.data.accessKey.accessToken);
-                this.$router.push({
-                    path: '/dashboard'
-                });
+                localStorage.setItem('otpToken', response.data.data.otpToken);
+                localStorage.setItem('email', this.email);
+                this.modalShow2 = true;
             }).catch(error => {
                 this.authError = error.response.data.message;
                 this.isAuthError = true;
                 this.processing = false;
-            });  
+            });
         },
 
         resendOtp(type) {
@@ -91,23 +102,30 @@ export default {
             });
         },
 
-        verifyOtp() {
-            axios.post(process.env.VUE_APP_API_URL + "/v1/auth/valid-otp", {
+        async verifyOtp() {
+            await axios.post(process.env.VUE_APP_API_URL + "/v1/auth/valid-otp", {
                 otp: this.otp.join(''),
                 otpToken: localStorage.getItem('otpToken')
-            }).then(response => {
+            }).then(async response => {
                 //set jwt
-
-                console.log("response", response.data.data);
                 if (this.typeRequestOtp == 'login') {
                     localStorage.setItem('jwt', response.data.data.accessToken);
-                    this.$router.push('/dashboard');
+                    
                 } else {
                     localStorage.setItem('forgotPasswordToken', response.data.data.resetPasswordToken);
                     this.$router.push('/reset-password');
                 }
             }).catch(() => {
                 this.otpError = true;
+            });
+
+            await axios.get(process.env.VUE_APP_API_URL + "/v1/me/menu", {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('jwt')}`
+                }
+            }).then(response => {
+                localStorage.setItem('menu', JSON.stringify(response.data.data));
+                this.$router.push('/dashboard');
             });
         },
 
@@ -169,22 +187,22 @@ export default {
     border-radius: 5px;
     margin: 0 5px;
 }
+
+.gradient-custom {
+    background: rgb(255, 213, 0);
+    background: radial-gradient(circle, rgba(255, 213, 0, 0.4458158263305322) 0%, rgba(255, 251, 229, 1) 100%);
+}
+
+.card-login {
+    border-radius: 4px;
+    padding: 60px 40px 20px 40px;
+    ;
+}
 </style>
 
-<template>
+<template class="">
 
-    <section class="vh-100">
-        <div class="container-fluid">
-            <div class="row">
-
-                <div class="col-sm-4 px-0 d-none d-sm-block vh-100 text-center align-items-center justify-content-center"
-                    style="background-color: black;">
-                    <img src="@/assets/images/logo.svg" alt="Logo" class="image-auth" />
-                    <div class="d-flex flex-column justify-content-center align-items-center"
-                        style="height: 70%;padding: 0px 64px;">
-                </div>
-            </div>
-        </div>
+    <section class="gradient-custom vh-100">
 
         <div class="container pt-4">
             <div class="d-flex justify-content-between align-items-center mb-4">
@@ -195,11 +213,11 @@ export default {
             </div>
         </div>
 
+        <section class="">
             <div class="container login-page">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                     <div class="col-12 col-md-8 col-lg-6 col-xl-6">
                         <div class="card bg-white text-dark ">
-                            <form>
                             <div class="card-body card-login">
 
                                 <div class="">
@@ -253,70 +271,16 @@ export default {
                                                 Akun</BLink>
                                         </p>
                                     </div>
-                                </div>
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example18">Email atau Nomor WhatsApp</label>
-                                    <input type="email" id="form2Example18" class="form-control form-control-lg" v-model="email" />
 
                                 </div>
 
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <label class="form-label" for="form2Example28">Password</label>
-                                    <div class="form-outline mb-4 position-relative">
-                                        <input :type="showPassword ? 'text' : 'password'" v-model="password"
-                                            id="form2Example28" class="form-control form-control-lg" />
-
-                                        <!-- Eye Icon -->
-                                        <i :class="showPassword ? 'ri-eye-line' : 'ri-eye-off-line'" @click="togglePassword"
-                                            style="position: absolute; right: 15px; top: 15px; cursor: pointer;"></i>
-                                    </div>
-                                </div>
-
-                                <div class="d-flex justify-content-between mb-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="rememberMe" />
-                                        <label class="form-check-label" for="rememberMe">Ingat saya</label>
-                                    </div>
-                                    <div>
-                                        <BLink href="javascript:void(0);" @click="modalShow1 = !modalShow1"
-                                            class="text-primary">Lupa password?
-                                        </BLink>
-                                    </div>
-                                </div>
-
-                            <div class="pt-1 mb-4">
-                                <button type="button"
-                                    class="btn btn-dark btn-lg btn-block w-100 waves-effect waves-light"
-                                    @click="signinapi" :disabled="processing">LOGIN</button>
                             </div>
                         </div>
-                        </form>
                     </div>
                 </div>
-                </div>
             </div>
-        </div>
+        </section>
 
-        <!-- Vertically Centered -->
-        <BModal v-model="modalShow1" hide-footer class="v-modal-custom" hide-header-close centered>
-            <div class="modal-body text-center">
-                <lottie colors="primary:#121331,secondary:#08a88a" trigger="loop" :options="defaultOptions"
-                    :height="120" :width="120" />
-                <div class="mt-4">
-                    <h4 class="mb-3 title-auth">Lupa password?</h4>
-                    <p class="text-muted mb-4">Silakan masukkan alamat email, Anda akan menerima Kode OTP untuk mengatur
-                        ulang kata sandi Anda.</p>
-                    <div class="vstack gap-5 justify-content-center">
-                        <BFormInput type="email" placeholder="Email" />
-                        <BButton type="button" variant="dark" @click="() => {
-                            modalShow1 = false;
-                            modalShow2 = true;
-                        }">
-                            Kirim OTP</BButton>
-                    </div>
-                </div>
-            </div>
-        </BModal>
 
         <BModal v-model="modalShow2" hide-footer class="v-modal-custom" centered>
             <div class="modal-body text-center">
@@ -339,12 +303,12 @@ export default {
 
                         <p class="text-muted mb-4">Tidak menerima kode?</p>
 
-                        <BLink href="javascript:void(0);" class="text-primary">Kirim ulang via email</BLink>
+                        <BLink @click="resendOtp('email')" class="text-primary">Kirim ulang via email</BLink>
 
-                        <BLink href="javascript:void(0);" class="text-primary">Kirim ulang via WhatsApp</BLink>
+                        <BLink @click="resendOtp('wa')" class="text-primary">Kirim ulang via WhatsApp</BLink>
 
 
-                        <router-link to="/reset-password" class="btn btn-dark">Verifikasi</router-link>
+                        <BButton @click="verifyOtp" class="btn btn-dark">Verifikasi</BButton>
                     </div>
                 </div>
             </div>
@@ -373,5 +337,4 @@ export default {
             </div>
         </BModal>
     </section>
-
 </template>
